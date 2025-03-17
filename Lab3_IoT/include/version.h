f64b462fc0e528b66aee033643654151901f0cfa
GeorgeABX <80110187+GeorgeABX@users.noreply.github.com>
2025-03-17 19:26:17 +0200

    Reverted lab 3

---
+++ Lab3_IoT/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "1"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.1 - 2025-03-03 18:57:57.265237"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.1"
+#endif
